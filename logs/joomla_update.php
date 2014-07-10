e6b5281b3a06ef693ffa670443ba6ebc4e27c0c2
Ricardo <ricardolyon@gmail.com>
2014-07-10 16:10:01 -0500

    primer commit

---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-07-10 16:41:08 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-07-10T16:41:08+00:00	INFO	update	Update started by user Super User (93). Old version is 3.3.0.
+2014-07-10T16:41:08+00:00	INFO	update	Downloading update file from .
+2014-07-10T16:41:10+00:00	INFO	update	File Joomla_3.3.x_to_3.3.1-Stable-Patch_Package.zip successfully downloaded.
+2014-07-10T16:41:11+00:00	INFO	update	Starting installation of new version.
+2014-07-10T16:41:19+00:00	INFO	update	Finalising installation.
+2014-07-10T16:41:20+00:00	INFO	update	Deleting removed files and folders.
+2014-07-10T16:41:35+00:00	INFO	update	Cleaning up after installation.
+2014-07-10T16:41:35+00:00	INFO	update	Update to version 3.3.1 is complete.
