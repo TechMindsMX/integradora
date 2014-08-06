172637c65531c8d1452a656b8a21b3af96c9111d
Ricardo <ricardolyon@gmail.com>
2014-08-06 15:58:38 -0500

    joomla ver 3.3.3

--- logs/joomla_update.php
+++ logs/joomla_update.php
@@ -12,3 +12,11 @@
 2014-07-10T16:41:20+00:00	INFO	update	Deleting removed files and folders.
 2014-07-10T16:41:35+00:00	INFO	update	Cleaning up after installation.
 2014-07-10T16:41:35+00:00	INFO	update	Update to version 3.3.1 is complete.
+2014-08-06T17:03:49+00:00	INFO	update	Actualización inciciadoa por el usuario Super User (93). La versión antigua es la 3.3.1.
+2014-08-06T17:03:49+00:00	INFO	update	Descargando el archivo desde .
+2014-08-06T17:03:52+00:00	INFO	update	El archivoe Joomla_3.3.x_to_3.3.3-Stable-Patch_Package.zip se ha descargado correctamente.
+2014-08-06T17:03:53+00:00	INFO	update	Iniciando la instalación de la versión nueva.
+2014-08-06T17:04:05+00:00	INFO	update	Finalizando la instalación.
+2014-08-06T17:04:06+00:00	INFO	update	Borrando los archivos y carpetas que han sido eliminados.
+2014-08-06T17:04:22+00:00	INFO	update	Limpieza posterior a la instalación.
+2014-08-06T17:04:22+00:00	INFO	update	La actualización a la versión 3.3.3 se ha completado.
