45f2ee69d0c30446c59b8a0b95558a2d88296154
Ricardo <ricardolyon@gmail.com>
2014-09-23 16:35:57 -0500

    listado de ODV

---
+++ components/com_mandatos/views/odvlist/view.html.php
@@ -0,0 +1,31 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.view');
+
+class MandatosViewOdvlist extends JViewLegacy {
+	function display($tpl = null){
+		$data 				= JFactory::getApplication()->input->getArray();
+		$this->integradoId	= $data['integradoId'];
+		
+		$this->data = $this->get('ordenes');
+		$this->token = getFromTimOne::token();
+		
+        if (count($errors = $this->get('Errors'))) {
+                JLog::add(implode('<br />', $errors), JLog::WARNING, 'jerror');
+                return false;
+        }
+		
+		$this->loadHelper('Mandatos');
+		
+		foreach ($this->data as $key => $odc) {
+			$odc->proveedor = MandatosHelper::getProviderFromID($odc->proveedor, $this->integradoId);
+			
+			$this->data[$key] = $odc;  
+		}
+		
+		$this->permisos = MandatosHelper::checkPermisos(__CLASS__, $this->integradoId);
+		
+		parent::display($tpl);
+	}
+}
