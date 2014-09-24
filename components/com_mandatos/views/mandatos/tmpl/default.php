8079622b1345ca36f4622e0c4718580dee2893fb
Ricardo <ricardolyon@gmail.com>
2014-09-24 12:14:13 -0500

    se elimina barra de cierre en tags a

--- components/com_mandatos/views/mandatos/tmpl/default.php
+++ components/com_mandatos/views/mandatos/tmpl/default.php
@@ -11,37 +11,37 @@ JHTML::_('behavior.calendar');
 echo '<h1>'.JText::_('COM_MANDATOS_TITULO').'</h1>';
 ?>
 <div style="margin-top: 50px;">
-    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=proyectoslist&integradoId='.$this->integradoId); ?>" />
+    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=proyectoslist&integradoId='.$this->integradoId); ?>">
         <?php echo JText::_('COM_MANDATOS_LISTAD_PROYECTOS'); ?>
     </a>
 </div>
 
 <div class="margen-fila">
-    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=productoslist&integradoId='.$this->integradoId); ?>" />
+    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=productoslist&integradoId='.$this->integradoId); ?>">
         <?php echo JText::_('COM_MANDATOS_LISTAD_PRODUCTOS'); ?>
     </a>
 </div>
 
 <div class="margen-fila">
-    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=clienteslist&integradoId='.$this->integradoId); ?>" />
+    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=clienteslist&integradoId='.$this->integradoId); ?>">
         <?php echo JText::_('COM_MANDATOS_LISTAD_CLIENTES'); ?>
     </a>
 </div>
 
 <div class="margen-fila">
-    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=odclist&integradoId='.$this->integradoId); ?>" />
+    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=odclist&integradoId='.$this->integradoId); ?>">
         <?php echo JText::_('COM_MANDATOS_LISTAD_ORDENES'); ?>
     </a>
 </div>
 
 <div class="margen-fila">
-    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=oddlist&integradoId='.$this->integradoId); ?>" />
+    <a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=oddlist&integradoId='.$this->integradoId); ?>">
         <?php echo JText::_('COM_MANDATOS_LISTAD_ORDENES_DEPOSITO'); ?>
     </a>
 </div>
 
 <div class="margen-fila">
-	<a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=odvlist&integradoId='.$this->integradoId); ?>" />
-	<?php echo JText::_('COM_MANDATOS_LISTAD_ORDENES_VENTA'); ?>
+	<a class="btn btn-primary" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=odvlist&integradoId='.$this->integradoId); ?>">
+		<?php echo JText::_('COM_MANDATOS_LISTAD_ORDENES_VENTA'); ?>
 	</a>
 </div>
