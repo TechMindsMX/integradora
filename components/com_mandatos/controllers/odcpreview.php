34d5debb8a1187b5701130857798a183462f90e3
Ricardo <ricardolyon@gmail.com>
2014-10-04 18:33:34 -0500

    subcontrollers correctos

--- components/com_mandatos/controllers/odcpreview.php
+++ components/com_mandatos/controllers/odcpreview.php
@@ -1,14 +1,19 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
+require_once JPATH_COMPONENT . '/helpers/mandatos.php';
+
 /**
  * metodo de envio a TimOne
+ * @property mixed parametros
+ * @property mixed app
+ * @property mixed permisos
  */
 class MandatosControllerOdcpreview extends JControllerAdmin {
 	
 	function authorize() {
 		$this->app 			= JFactory::getApplication();
-		$this->paramertros	= $this->app->input->getArray();
+		$this->parametros	= $this->app->input->getArray();
 
         $this->permisos     = MandatosHelper::checkPermisos(__CLASS__, $this->parametros['integradoId']);
 
--- components/com_mandatos/views/odvpreview/tmpl/confirmauth_confirm_btns.php
+++ components/com_mandatos/views/odvpreview/tmpl/confirmauth_confirm_btns.php
@@ -1,23 +1,24 @@
- <?php 
+<?php
 defined('_JEXEC') or die('Restricted access');
 
-$returnUrl = JRoute::_('index.php?option=com_mandatos&view=odvlist&integradoId='.$this->integradoId);
+$returnUrl = JRoute::_('index.php?option=com_mandatos&view=odvlist&integradoId=' . $this->integradoId);
 ?>
 
-        <legend class="container botones clearfix form-actions">
-        <?php
-        if($this->permisos['canAuth'] && $this->odv->status === 0 ):
-            $authorizeURL = JRoute::_('index.php?option=com_mandatos&view=odvpreview&task=authorize&integradoId='.$this->integradoId.'&odvnum='.$this->odv->id);
-        ?>
-	        <p class="text-warning">
+<legend class="container botones clearfix form-actions">
+	<?php
+	if ($this->permisos['canAuth'] && $this->odv->status === 0):
+		$authorizeURL = JRoute::_('index.php?option=com_mandatos&view=odvpreview&task=odvpreview.authorize&integradoId=' . $this->integradoId . '&odvnum=' . $this->odv->id);
+		?>
+		<p class="text-warning">
 		        <span>
-			        <input type="checkbox" class="" id="authorize" />
+			        <input type="checkbox" class="" id="authorize"/>
                  </span>
-		        <?php echo JText::_('LBL_CONFIRM_AUTH'); ?>
-	        </p>
-            <a id="authorize-btn" class="btn btn-success esconder" href="<?php echo $authorizeURL ?>"><?php echo JText::_('LBL_AUTORIZE'); ?></a>
-        <?php
-        endif;
-        ?>
-            <a class="btn btn-danger" href="<?php echo $returnUrl; ?>"><?php echo JText::_('LBL_CANCELAR'); ?></a>
-        </legend>
+			<?php echo JText::_('LBL_CONFIRM_AUTH'); ?>
+		</p>
+		<a id="authorize-btn" class="btn btn-success esconder"
+		   href="<?php echo $authorizeURL ?>"><?php echo JText::_('LBL_AUTORIZE'); ?></a>
+	<?php
+	endif;
+	?>
+	<a class="btn btn-danger" href="<?php echo $returnUrl; ?>"><?php echo JText::_('LBL_CANCELAR'); ?></a>
+</legend>
