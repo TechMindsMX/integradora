e71e3fc09d86d4a437108e6fb70192208c9d08d5
Ricardo <ricardolyon@gmail.com>
2014-10-07 15:29:09 -0500

    formulario de alta de comisión

--- administrator/components/com_adminintegradora/controllers/comision.php
+++ administrator/components/com_adminintegradora/controllers/comision.php
@@ -2,7 +2,6 @@
 defined ('_JEXEC') or die('Restricted Access');
 
 jimport ('joomla.application.component.controlleradmin');
-jimport('integradora.validator');
 
 /**
  *
@@ -23,20 +22,5 @@ class AdminintegradoraControllerComision extends JControllerAdmin
 		JFactory::getApplication ()->redirect ($url);
 	}
 
-	public function savecomision () {
-		$envio = JFactory::getApplication ()->input->getArray (array('description' => 'string', 'type' => 'string', 'monto' => 'string', 'rate' => 'float', 'frequencyTimes' => 'string'));
-
-		$diccionario = array ('description' 	=> array ('tipo' => 'alfaNum', 'label' => JText::_ ('LBL_INTEGRADO_ID'), 'length' => 255),
-							  'type' 			=> array ('tipo' => 'number', 'label' => JText::_ ('LBL_INTEGRADO_ID'), 'length' => 10),
-							  'monto' 			=> array ('tipo' => 'number', 'label' => JText::_ ('LBL_INTEGRADO_ID'), 'length' => 10),
-							  'rate' 			=> array ('tipo' => 'rate', 'label' => JText::_ ('LBL_INTEGRADO_ID'), 'length' => 10),
-							  'frequencyTimes' 	=> array ('tipo' => 'number', 'label' => JText::_ ('LBL_INTEGRADO_ID'), 'length' => 10)
-		);
-
-		$validaResult = validador::procesamiento($envio, $diccionario);
-
-		var_dump($validaResult);
-
-	}
 
 }
