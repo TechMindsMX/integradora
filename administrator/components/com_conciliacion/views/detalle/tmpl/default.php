a92859605b5f902f12abc10c6b856475f9c5a304
Ricardo <ricardolyon@gmail.com>
2014-10-22 17:03:06 -0500

    comienza a pintar datos, tabla asocia Tx a mandatos

--- administrator/components/com_conciliacion/views/detalle/tmpl/default.php
+++ administrator/components/com_conciliacion/views/detalle/tmpl/default.php
@@ -1,9 +1,34 @@
 <?php
 // no direct access
-defined('_JEXEC') or die;
+defined( '_JEXEC' ) or die;
 
-var_dump($this->item);
+
+
+?>
+
+<script type="text/javascript">
+	jQuery(document).ready(function ($) {
+		var odv = <?php echo $this->odv; ?>;
+		var odc = <?php echo $this->odc; ?>;
+		var odd = <?php echo $this->odd; ?>;
+		var odr = <?php echo $this->odr; ?>;
+
+		var $integ = $('#integrado').change(function() {
+			filterTable($integ);
+		});
+	});
+</script>
+
+<label for="integrado"><?php echo JText::_('LBL_INTEGRADO');?><input name="integrado" id="integrado" type="text"/></label>
+<label for="mandato"><?php echo JText::_('LBL_MANDATO');?><input name="mandato" id="mandato" type="text"/></label>
+
+
+<?php
+$html = null;
+foreach ( $this->odv as $odv ) {
+	$html = '<div class="span4">'.JText::_($odv->id).'</div>';
+	$html = '<div class="span4">'.$odv->id.'</div>';
+}
+
+echo $html;
 var_dump($this->odv);
-var_dump($this->odc);
-var_dump($this->odd);
-var_dump($this->odr);
