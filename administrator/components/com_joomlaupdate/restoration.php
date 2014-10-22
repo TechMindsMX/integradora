ef7b154664770d3272ed2c3b8cf1b75e8f069508
Ricardo <ricardolyon@gmail.com>
2014-10-22 15:30:35 -0500

    archivo no versionado de actualizacion joomla

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'wWsdr7SA9t9lkalCdEtR2Ml70SdbWvuH',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/var/www/html/trama/integradora/tmp/Joomla_3.3.x_to_3.3.3-Stable-Patch_Package.zip',
+	'kickstart.setup.destdir' => '/var/www/html/trama/integradora',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
