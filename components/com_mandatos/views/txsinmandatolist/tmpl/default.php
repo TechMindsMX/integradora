7ee3235935ed3a327856c55c7b66847bf698f4ba
Ricardo <ricardolyon@gmail.com>
2014-12-05 17:05:48 -0600

    botones de asociar a mandato y nuevo mandato

--- components/com_mandatos/views/txsinmandatolist/tmpl/default.php
+++ components/com_mandatos/views/txsinmandatolist/tmpl/default.php
@@ -7,17 +7,18 @@ jimport('joomla.html.html.bootstrap');
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidation');
 JHTML::_('behavior.calendar');
-
-
-var_dump($this->txs);
-
+$btn_nuevo = '<a class="btn btn-primary" href="index.php?option=com_mandatos&integradoId='.$this->integradoId.'">'.JText::_('COM_MANDATOS_LIST_TX_BTN_NUEVO_MANDATO').'</a>';
 
 ?>
 
 <h1><?php echo JText::_('COM_MANDATOS_LIST_TX_SIN_MANDATO_TITLE'); ?></h1>
 
 <div class="table-responsive">
+	<div class="form-group">
+		<?php echo $btn_nuevo; ?>
+	</div>
 	<table id="myTable" class="table table-bordered tablesorter">
+
 		<thead>
 		<tr>
 			<th style="text-align: center; vertical-align: middle;" ><span class="etiqueta"><?php echo JText::_('COM_MANDATOS_LIST_TX_REF'); ?></span> </th>
@@ -31,20 +32,18 @@ var_dump($this->txs);
 		<?php
 		if( !is_null($this->txs) ){
 			foreach ($this->txs as $key => $value) {
-				$url_preview = JRoute::_('index.php?option=com_mandatos&view=txsinmandato&integradoId='.$this->integradoId.'&facturanum='.$value->id);
-				$preview_button = '<a href="'.$url_preview.'"><i class="icon-search"></i></a>';
-				$btn = '<a class="btn btn-success" href="index.php?option=com_mandatos&view=facturapreview&layout=confirmcancel&facturanum='.$value->id.'&integradoId='.$this->integradoId.'">'.JText::_('COM_MANDATOS_ORDENES_CANCEL_FACT').'</a>';
+				$btn_asoociar = '<a class="btn btn-success" href="index.php?option=com_mandatos&view=txsinmandato&layout=confirml&txnum='.$value->id.'&integradoId='.$this->integradoId.'">'.JText::_('COM_MANDATOS_LIST_TX_BTN_ASOCIAR').'</a>';
 
 				echo '<tr class="row_'.$value->id.'">';
 				echo '	<td style="text-align: center; vertical-align: middle;" class="margen-fila" >'.$value->referencia.'</td>';
 				echo '	<td style="text-align: center; vertical-align: middle;" class="" >'.$value->cuenta.'</td>';
 				echo '	<td style="text-align: center; vertical-align: middle;" class="" >'.$value->date.'</td>';
 				echo '	<td style="text-align: center; vertical-align: middle;" class="" >$'.number_format($value->amount,2).'</td>';
-				echo '	<td style="text-align: center; vertical-align: middle;" class="" >'.$btn.'</td>';
+				echo '	<td style="text-align: center; vertical-align: middle;" class="" >'.$btn_asoociar.'</td>';
 				echo '</tr>';
 			}
 		}else{
-			JFactory::getApplication()->enqueueMessage(JText::_('COM_MANDATOS_LIST_NO_TXLIST'));
+			JFactory::getApplication()->enqueueMessage(JText::_('COM_MANDATOS_LIST_TX_NO_TXLIST'));
 		}
 		?>
 		</tbody>
