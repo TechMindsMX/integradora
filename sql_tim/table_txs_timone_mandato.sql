cc916f6a2dc5abaa39a38cb6d3ce25c8200a9604
Ricardo <ricardolyon@gmail.com>
2014-12-09 16:55:07 -0600

    idOrden DEFAULT NULL en txs_timone_mandatos

--- sql_tim/table_txs_timone_mandato.sql
+++ sql_tim/table_txs_timone_mandato.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `flpmu_txs_timone_mandato`;
 CREATE TABLE `flpmu_txs_timone_mandato` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idTx` int(11) NOT NULL,
-  `idOrden` int(11) NOT NULL,
+  `idOrden` int(11) DEFAULT NULL,
   `idIntegrado` int(11) NOT NULL,
   `date` bigint(20) NOT NULL,
   `tipoOrden` varchar(45) DEFAULT NULL,
