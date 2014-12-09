66173d5376c3f1f1afe60583f859720df9c1511a
Ricardo <ricardolyon@gmail.com>
2014-12-09 16:48:06 -0600

    cambio tabla `flpmu_txs_timone_mandato`

--- sql_tim/table_txs_timone_mandato.sql
+++ sql_tim/table_txs_timone_mandato.sql
@@ -1,8 +1,12 @@
+DROP TABLE IF EXISTS `flpmu_txs_timone_mandato`;
+
 CREATE TABLE `flpmu_txs_timone_mandato` (
-  `idComision` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idTx` int(11) NOT NULL,
   `idOrden` int(11) NOT NULL,
   `idIntegrado` int(11) NOT NULL,
-  `tipoOrden` varchar(45) NOT NULL,
-  `date` bigint(20) NOT NULL
+  `date` bigint(20) NOT NULL,
+  `tipoOrden` varchar(45) DEFAULT NULL,
+  `idComision` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
