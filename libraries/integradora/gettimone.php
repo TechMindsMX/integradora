41f486ab296c8824efada0c3daf91b8881541ce8
Ricardo <ricardolyon@gmail.com>
2014-12-10 12:15:31 -0600

    ajustes tablas

--- libraries/integradora/gettimone.php
+++ libraries/integradora/gettimone.php
@@ -85,7 +85,7 @@ class getFromTimOne{
 
     public static function getTxConciliaciones($where){
 
-        $txs = self::selectDB('conciliacion_banco_integrado',$where,'','getFromTimOne');
+        $txs = self::selectDB('txs_banco_integrado',$where,'','getFromTimOne');
 
         foreach ($txs as $value) {
             $value->id              = (INT) $value->id;
--- sql_tim/changelog_integradora.sql
+++ sql_tim/changelog_integradora.sql
@@ -1,15 +1,2 @@
 --liquibase formatted sql
 
---changeset ricardolyon:1
-create table person (
-  id int not null primary key,
-  firstname varchar(80),
-  lastname varchar(80) not null,
-  state varchar(2)
-);
-
---rollback drop table person
-
---changeset ricardolyon:2
-drop table person;
-
