78725b3e8c7369538eb3e651c22278679e5f61b9
Ricardo <ricardolyon@gmail.com>
2014-12-11 12:21:11 -0600

    filtros por tipo de orden

--- administrator/components/com_conciliacion/views/detalle/tmpl/default.php
+++ administrator/components/com_conciliacion/views/detalle/tmpl/default.php
@@ -15,58 +15,86 @@ defined( '_JEXEC' ) or die;
 				filtro_fechas($integ);
 			});
 			$('#filtrofecha').on('click', filtro_fechas );
+
+			// filto por tipo de orden
+			$('input[id*="table_"]').click(function() {
+
+				var $id = $(this).prop('id');
+
+				var $tablas = $('#tablas_datos .table');
+
+				$.each($tablas, function(k, v) {
+					if(v.id == $id) {
+						$(v).show();
+					} else {
+						$(v).hide();
+					}
+				});
+			});
 		});
 	</script>
 
-	<div class="filtros" id="columna1" >
-		<label for="integrado">Seleciona el Integrado:</label>
-		<select id='integrado' name="integrado" class="integrado">
-			<option value="0" selected="selected"><?php JText::_('LBL_SELECCIONE'); ?></option>
-			<?php
-			foreach ($this->integrados as $key => $value) {
-				echo '<option value="'.$value->integrado_id.'">'.$value->name.'</option>';
-			}
-			?>
-		</select>
-	</div>
-	<div class="filtros">
-		<div class="columna1">
-			<label for="fechaFin"><?php JText::_('LBL_FECHA_INICIO'); ?></label>
-			<?php
-			$d = new DateTime();
-			$d->modify('first day of this month');
-			$default = $d->format('Y-m-d');
-			$attsCal = array('class'=>'inputbox forceinline', 'size'=>'25', 'maxlength'=>'19');
-			echo JHTML::_('calendar',$default,'fechaInicio', 'fechaInicio', $format = '%Y-%m-%d', $attsCal);
-			?>
-		</div>
-		<div class="columna1">
-			<label for="fechaFin"><?php JText::_('LBL_FECHA_FIN'); ?></label>
-			<?php
-			$d = new DateTime();
-			$d->modify('last day of this month');
-			$default = $d->format('Y-m-d');
-			echo JHTML::_('calendar',$default,'fechaFin', 'fechaFin', $format = '%Y-%m-%d', $attsCal);
-			?>
-		</div>
-		<div>
-			<input type="button" class="btn btn-primary" value="Buscar" id="filtrofecha">
-			<input type="button" class="btn btn-primary" value="Limpiar" id="llenatabla">
-		</div>
-	</div>
-	<div class="tipoOrden">
-		<div class="form-horizontal form-actions">
-			<a class="btn btn-success"><?php echo JText::_('LBL_ODVS'); ?></a>
-			<a class="btn btn-success"><?php echo JText::_('LBL_ODCS'); ?></a>
-			<a class="btn btn-success"><?php echo JText::_('LBL_ODDS'); ?></a>
-		</div>
-	</div>
+<div class="clearfix top">
+	<div class="span6">
 
+		<div class="filtros" id="columna1" >
+			<label for="integrado">Seleciona el Integrado:</label>
+			<select id='integrado' name="integrado" class="integrado">
+				<option value="0" selected="selected"><?php JText::_('LBL_SELECCIONE'); ?></option>
+				<?php
+				foreach ($this->integrados as $key => $value) {
+					echo '<option value="'.$value->integrado_id.'">'.$value->name.'</option>';
+				}
+				?>
+			</select>
+		</div>
+		<div class="filtros">
+			<div class="columna1">
+				<label for="fechaFin"><?php JText::_('LBL_FECHA_INICIO'); ?></label>
+				<?php
+				$d = new DateTime();
+				$d->modify('first day of this month');
+				$default = $d->format('Y-m-d');
+				$attsCal = array('class'=>'inputbox forceinline', 'size'=>'25', 'maxlength'=>'19');
+				echo JHTML::_('calendar',$default,'fechaInicio', 'fechaInicio', $format = '%Y-%m-%d', $attsCal);
+				?>
+			</div>
+			<div class="columna1">
+				<label for="fechaFin"><?php JText::_('LBL_FECHA_FIN'); ?></label>
+				<?php
+				$d = new DateTime();
+				$d->modify('last day of this month');
+				$default = $d->format('Y-m-d');
+				echo JHTML::_('calendar',$default,'fechaFin', 'fechaFin', $format = '%Y-%m-%d', $attsCal);
+				?>
+			</div>
+			<div>
+				<input type="button" class="btn btn-primary" value="Buscar" id="filtrofecha">
+				<input type="button" class="btn btn-primary" value="Limpiar" id="llenatabla">
+			</div>
+		</div>
+		<div class="tipoOrden">
+			<div class="form-horizontal form-actions" id="order_type_filter">
+				<input type="button" class="btn btn-success" id="table_odv" value="<?php echo JText::_('LBL_ODVS'); ?>" />
+				<input type="button" class="btn btn-success" id="table_odc" value="<?php echo JText::_('LBL_ODCS'); ?>" />
+				<input type="button" class="btn btn-success" id="table_odd" value="<?php echo JText::_('LBL_ODDS'); ?>" />
+			</div>
+		</div>
+	</div>
+	<div id="datosTx" class="span6">
+		<label><b><?php echo JText::_('LBL_TXID'); ?></b></label><span class=""><?php echo $this->item->id; ?></span>
+		<label><b><?php echo JText::_('LBL_INTEGRADO'); ?></b></label><span class=""><?php echo $this->item->idIntegrado; ?></span>
+		<label><b><?php echo JText::_('LBL_REFERENCIA'); ?></b></label><span class=""><?php echo $this->item->data->referencia; ?></span>
+		<label><b><?php echo JText::_('LBL_FECHA'); ?></b></label><span class=""><?php echo date("d-m=Y", $this->item->data->date); ?></span>
+		<label><b><?php echo JText::_('LBL_MONTO'); ?></b></label><span class=""><?php echo number_format($this->item->data->amount, 2); ?></span>
+	</div>
+</div>
 
+<div id="tablas_datos">
 <?php
 $url_asociar = 'index.php?option=com_conciliacion&view=detalle&layout=confirm&orderId=';
 
-$html[] = '<div class=""><h3>'.JText::_('LBL_ODVS').'</h3>';
+$html[] = '<div class="table table-striped" id="table_odv"><h3>'.JText::_('LBL_ODVS').'</h3>';
 foreach ( $this->odv as $orden) {
 	$btn_asociar = '<a class="btn btn-success" href="'.$url_asociar.$orden->id.'&orderType=odv">'.JText::_('LBL_ASOCIAR').'</a>';
 	$html[] = '<div class="row1 clearfix">';
@@ -82,7 +110,7 @@ foreach ( $this->odv as $orden) {
 }
 $html[] = '</div>';
 
-$html[] = '<div class=""><h3>'.JText::_('LBL_ODCS').'</h3>';
+$html[] = '<div class="table table-striped" id="table_odc"><h3>'.JText::_('LBL_ODCS').'</h3>';
 foreach ( $this->odc as $orden) {
 	$btn_asociar = '<a class="btn btn-success" href="'.$url_asociar.$orden->id.'&orderType=odc">'.JText::_('LBL_ASOCIAR').'</a>';
 	$html[] = '<div class="row1 clearfix">';
@@ -98,7 +126,7 @@ foreach ( $this->odc as $orden) {
 }
 $html[] = '</div>';
 
-$html[] = '<div class=""><h3>'.JText::_('LBL_ODDS').'</h3>';
+$html[] = '<div class="table table-striped" id="table_odd"><h3>'.JText::_('LBL_ODDS').'</h3>';
 foreach ( $this->odd as $orden ) {
 	$btn_asociar = '<a class="btn btn-success" href="'.$url_asociar.$orden->id.'&orderType=odd">'.JText::_('LBL_ASOCIAR').'</a>';
 	$html[] = '<div class="row1 clearfix">';
@@ -117,5 +145,6 @@ $html[] = '</div>';
 
 echo implode('',$html);
 
-
+?>
+</div>
 //var_dump($this->odd, $this->odc, $this->odv);
