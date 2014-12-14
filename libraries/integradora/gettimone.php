6d7f118edd92cb1ce8da607daa693dc01a827aed
Ismael Aguilar <aguilar_2001@hotmail.com>
2014-12-13 23:45:59 -0600

    Creación de Tabla de amortizacion de cuota fija

--- libraries/integradora/gettimone.php
+++ libraries/integradora/gettimone.php
@@ -118,6 +118,93 @@ class getFromTimOne{
     }
 
     public static function getTabla($data){
+
+        /*
+         * object(stdClass)#354 (4) {
+         *  ["tiempoplazo"]=>
+         *  float(12)
+         *  ["tipoPlazo"]=>
+         *  float(2)
+         *  ["capital"]=>
+         *  float(20000)
+         *  ["interes"]=>
+         *  float(3)
+         * }
+         * */
+
+        $tabla= new stdClass();
+        $tabla->coniva      = $data->interes*1.16;
+        $tabla->saldo       = $data->capital;
+        switch($data->tipoPlazo){
+            case 1:
+                $tabla->tperiodo    = 'Diaria';
+                $tabla->periodos    = '365';
+                break;
+            case 2:
+                $tabla->tperiodo    = 'Quincenal';
+                $tabla->periodos    = '104';
+                break;
+            case 3:
+                $tabla->tperiodo    = 'Mensual';
+                $tabla->periodos    = '12';
+                break;
+            case 4:
+                $tabla->tperiodo    = 'Bimestral';
+                $tabla->periodos    = '6';
+                break;
+            case 5:
+                $tabla->tperiodo    = 'Trimestral';
+                $tabla->periodos    = '4';
+                break;
+            case 6:
+                $tabla->tperiodo    = 'Semestral';
+                $tabla->periodos    = '2';
+                break;
+            case 7:
+                $tabla->tperiodo    = 'Anual';
+                $tabla->periodos    = '1';
+                break;
+            default:
+                break;
+        }
+
+        $temp           = $tabla->coniva/100;
+        $temp           = $temp/$data->tiempoplazo;
+        $temp           = $temp+1;
+        $temp           = pow($temp, $data->tiempoplazo);
+        $temp           = $temp-1;
+        $temp           = $temp*$data->tiempoplazo;
+
+        $tabla->tepp    = number_format($temp*100, 2,'.', ',');
+        $tabla->ccf     = number_format($data->capital/$data->tiempoplazo, 2 );
+        $final=$tabla->saldo;
+        $capital=round($tabla->ccf);
+        for($i = 1; $i <= $data->tiempoplazo; $i++ ){
+            $inicial              = $final;
+            $intiva               = $inicial*($tabla->coniva/100);
+
+            $intereses            = $intiva/1.16;
+            $iva                  = $intereses*0.16;
+            $cuota                = $intiva+$tabla->ccf;
+            $final                = $inicial-$tabla->ccf;
+
+            $tabla->amortizacion[]= array(
+                'periodo'       => $i,
+                'inicial'       => $inicial,
+                'cuota'         => $cuota,
+                'intiva'        => $intiva,
+                'intereses'     => $intereses,
+                'iva'           => $iva,
+                'acapital'      => round($tabla->ccf),
+                'final'         => $final
+            );
+
+
+        }
+        $return = new stdClass();
+        $return->cf     = $tabla;
+        $return->cu     = '';
+        var_dump($return);
         $tabla = $data;
         return $tabla;
     }
