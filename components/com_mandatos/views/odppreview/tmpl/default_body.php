8a114c44490c0d2f00d2f76899c6d8f877925210
lutekValhalla <lutek.valhalla@gmail.com>
2015-01-08 16:38:31 -0600

    vista previa de ODP dado un id de mutuo

--- components/com_mandatos/views/odppreview/tmpl/default_body.php
+++ components/com_mandatos/views/odppreview/tmpl/default_body.php
@@ -28,8 +28,6 @@ $orden = $this->orden;
         <div class="span6">Fecha de depósito: <strong><?php echo $orden->fecha_deposito; ?></strong></div>
     </div>
 
-    <div class="clearfix">&nbsp;</div>
-
     <div>
         <div class="span6">Tasa: <strong><?php echo $orden->tasa ?></strong></div>
         <div class="span6">Tipo Movimiento: <strong><?php echo $orden->tipo_movimiento; ?></strong></div>
@@ -42,11 +40,9 @@ $orden = $this->orden;
         <div class="span6">Capital: <strong>-$<?php echo number_format($orden->capital,2); ?></strong></div>
     </div>
 
-    <div class="clearfix">&nbsp;</div>
-
     <div>
         <div class="span6">RFC: <strong><?php echo $orden->a_rfc; ?></strong></div>
-        <div class="span6">Intereses: <strong>-</strong></div>
+        <div class="span6">Intereses: <strong>-$<?php echo number_format($orden->intereses,2); ?></strong></div>
         <div class="clearfix"></div>
         <div class="span7" style="text-align: right;">IVA: <strong>$<?php echo number_format($orden->iva_intereses,2); ?></strong></div>
     </div>
@@ -58,17 +54,12 @@ $orden = $this->orden;
         <div class="span6">Capital: <strong>$<?php echo number_format($orden->capital,2); ?></strong></div>
     </div>
 
-    <div class="clearfix">&nbsp;</div>
-
     <div>
         <div class="span6">RFC: <strong><?php echo $orden->d_rfc; ?></strong></div>
-        <div class="span6">interese: <strong>-</strong></div>
+        <div class="span6">interese: <strong>$<?php echo number_format($orden->intereses,2); ?></strong></div>
         <div class="clearfix"></div>
         <div class="span7" style="text-align: right;">IVA: <strong>$<?php echo number_format($orden->iva_intereses,2); ?></strong></div>
     </div>
-
-    <div class="clearfix">&nbsp;</div>
-
 
     <div>
         <div class="span6"><h3>Importe de la cantidad:</h3> <h4>$<?php echo number_format($orden->capital,2); ?></h4></div>
@@ -76,5 +67,28 @@ $orden = $this->orden;
     </div>
 
     <div class="clearfix">&nbsp;</div>
+
+    <div class="clearfix" style="margin-top: 40px;">
+        AUTORIZO EXPRESAMENTE A INTEGRADORA DE EMPRENDIMIENTOS CULTURALES, S.A. DE C.V.,CONFORME A LOS ESTATUTOS, POLITICAS,
+        REGLAS Y PROCEDIMIENTOS, A EFECTUAR LA OPERACIÓN DE PRÉSTAMO DESCRITA Y A NOMBRE Y CUENTA DE MI REPRESENTADA
+
+        <table class="table-condensed table-mutuo">
+
+            <tr>
+                <td>_____________________________</td>
+                <td>&nbsp;</td>
+                <td>____________________________</td>
+            </tr>
+            <tr>
+                <td>(Nombre y Firma)<br />
+                    Apoderado Legal<br />
+                    Acreedor</td>
+                <td>&nbsp;</td>
+                <td>(Nombre y Firma)<br />
+                    Apoderado Legal<br />
+                    Deudor</td>
+            </tr>
+        </table>
+    </div>
 
 </div>
