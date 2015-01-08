5e3855393f823f35938f56f9c26687eb5149e851
lutekValhalla <lutek.valhalla@gmail.com>
2015-01-08 15:58:47 -0600

    vista previa de ODP dado un id de mutuo

--- components/com_mandatos/views/odppreview/tmpl/default_body.php
+++ components/com_mandatos/views/odppreview/tmpl/default_body.php
@@ -7,7 +7,8 @@ jimport('joomla.html.html.bootstrap');
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidation');
 JHTML::_('behavior.calendar');
-
+jimport('integradora.numberToWord');
+$number2word = new AifLibNumber;
 $orden = $this->orden;
 ?>
 
@@ -26,37 +27,54 @@ $orden = $this->orden;
         <div class="span6">Fecha de Elaboración: <strong><?php echo date('d-m-Y', $orden->fecha_elaboracion) ?></strong></div>
         <div class="span6">Fecha de depósito: <strong><?php echo $orden->fecha_deposito; ?></strong></div>
     </div>
+
     <div class="clearfix">&nbsp;</div>
+
     <div>
         <div class="span6">Tasa: <strong><?php echo $orden->tasa ?></strong></div>
         <div class="span6">Tipo Movimiento: <strong><?php echo $orden->tipo_movimiento; ?></strong></div>
     </div>
+
     <div class="clearfix">&nbsp;</div>
+
     <div>
         <div class="span6">Acreedor: <strong><?php echo $orden->acreedor ?></strong></div>
         <div class="span6">Capital: <strong>-$<?php echo number_format($orden->capital,2); ?></strong></div>
     </div>
+
     <div class="clearfix">&nbsp;</div>
+
     <div>
         <div class="span6">RFC: <strong><?php echo $orden->a_rfc; ?></strong></div>
         <div class="span6">Intereses: <strong>-</strong></div>
         <div class="clearfix"></div>
         <div class="span7" style="text-align: right;">IVA: <strong>$<?php echo number_format($orden->iva_intereses,2); ?></strong></div>
     </div>
+
     <div class="clearfix">&nbsp;</div>
+
     <div>
         <div class="span6">Deudor: <strong><?php echo $orden->deudor; ?></strong></div>
-        <div class="span6">Capital: <strong><?php echo number_format($orden->capital,2); ?></strong></div>
+        <div class="span6">Capital: <strong>$<?php echo number_format($orden->capital,2); ?></strong></div>
     </div>
+
     <div class="clearfix">&nbsp;</div>
+
     <div>
         <div class="span6">RFC: <strong><?php echo $orden->d_rfc; ?></strong></div>
         <div class="span6">interese: <strong>-</strong></div>
         <div class="clearfix"></div>
         <div class="span7" style="text-align: right;">IVA: <strong>$<?php echo number_format($orden->iva_intereses,2); ?></strong></div>
     </div>
+
     <div class="clearfix">&nbsp;</div>
 
-    <div></div>
+
+    <div>
+        <div class="span6"><h3>Importe de la cantidad:</h3> <h4>$<?php echo number_format($orden->capital,2); ?></h4></div>
+        <div class="span6"><h3>Importe en letra:</h3><h4><?php echo $number2word->toCurrency('$'.number_format($orden->capital,2)); ?></h4></div>
+    </div>
+
+    <div class="clearfix">&nbsp;</div>
 
 </div>
