727f610eef653087c24785b8f7bb9c646a9550af
Nestor Aguiar <nestor.aguilar@trama.mx>
2015-01-13 02:04:43 -0600

    Notificacion 11-12

--- components/com_mandatos/controllers/odcform.raw.php
+++ components/com_mandatos/controllers/odcform.raw.php
@@ -3,6 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport('integradora.gettimone');
 jimport('integradora.validator');
+jimport('integradora.notifications');
 require_once JPATH_COMPONENT . '/helpers/mandatos.php';
 
 class MandatosControllerOdcform extends JControllerLegacy {
@@ -73,38 +74,35 @@ class MandatosControllerOdcform extends JControllerLegacy {
 
 
             /*NOTIFICACIONES 11*/
+
+            $currentIntegradoId= JFactory::getSession()->get('integradoId', null, 'integrado');
+            $int = new IntegradoSimple($currentIntegradoId);
+
             $titulo = JText::_('TITULO_11');
-            $titulo = str_replace('$idOrden', '<strong style="color: #000000">'.$data['id_orden'].'</strong>',$titulo);
+            $titulo = str_replace('$idOrden', '<strong style="color: #000000">'.$this->parametros['idOrden'].'</strong>',$titulo);
 
             $contenido = JText::_('NOTIFICACIONES_11');
-
-            $contenido = str_replace('$integrado', '<strong style="color: #000000">'.$data['nameIntegrado'].'</strong>',$contenido);
-            $contenido = str_replace('$usuario', '<strong style="color: #000000">$'.$data['corrUser'].'</strong>',$contenido);
-            $contenido = str_replace('$idOrden', '<strong style="color: #000000">$'.$data['id_orden'].'</strong>',$contenido);
-            $contenido = str_replace('$cliente', '<strong style="color: #000000">$'.$data['cliente'].'</strong>',$contenido);
+            $contenido = str_replace('$integrado', '<strong style="color: #000000">'.$int->user->username.'</strong>',$contenido);
+            $contenido = str_replace('$usuario', '<strong style="color: #000000">'.$int->user->username.'</strong>',$contenido);
+            $contenido = str_replace('$idOrden', '<strong style="color: #000000">'.$this->parametros['idOrden'].'</strong>',$contenido);
+            $contenido = str_replace('$cliente', '<strong style="color: #000000">'.$data['cliente'].'</strong>',$contenido);
             $contenido = str_replace('$fecha', '<strong style="color: #000000">'.date('d-m-Y').'</strong>',$contenido);
-            $contenido = str_replace('$monto', '<strong style="color: #000000">'.$data('monto').'</strong>',$contenido);
-            $contenido = str_replace('$odv', '<strong style="color: #000000">'.$data('odv').'</strong>',$contenido);
+            $contenido = str_replace('$odv', '<strong style="color: #000000">'.$data['odv'].'</strong>',$contenido);
 
-            $integrado              = new IntegradoSimple($this->integradoId);
-
-            $data['corrUser']       = $this->currUser->name;
             $data['titulo']         = $titulo;
-            $data['nameIntegrado']  = $integrado->getDisplayName();
             $data['body']           = $contenido;
 
             $send                   = new Send_email();
             $send->notification($data);
 
-            if ($send->isError()){
+
+            /*if ($send->isError()){
                 $resp = $send->getErrorMsg();
                 $this->app->enqueueMessage($send->getErrorMsg());
             }else{
                 $resp = 'enviado';
                 $this->app->enqueueMessage('Correos enviados');
-            }
-            var_dump( $resp);exit;
-
+            }*/
         }else{
             $respuesta = array('redireccion' => false);
         }
