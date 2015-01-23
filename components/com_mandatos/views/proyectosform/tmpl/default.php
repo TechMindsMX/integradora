7b5a662e85c5a66e7ee103f61d9b29eebb50f740
lutekValhalla <lutek.valhalla@gmail.com>
2015-01-23 12:49:04 -0600

    no mostrar el Boton de limpiar cuando es edicion

--- components/com_mandatos/views/proyectosform/tmpl/default.php
+++ components/com_mandatos/views/proyectosform/tmpl/default.php
@@ -54,29 +54,30 @@ $proyecto = $this->proyecto;
 <form id="form_alta" method="post" action="">
 	<h1 style="margin-bottom: 40px;"><?php echo JText::_($this->titulo); ?></h1>
 
-    <input type="hidden" name="confirm" 	value="1" />
-    <input type="hidden" name="integradoId" value="<?php echo $proyecto->integradoId; ?>">
-    <input type="hidden" name="status" 		value="<?php echo $proyecto->status ?>">
-    <input type="hidden" name="parentId" 	value="<?php echo $proyecto->parentId ?>">
-    <input type="hidden" name="id_proyecto" value="<?php echo $proyecto->id_proyecto ?>">
+	<input type="hidden" name="confirm" 	value="1" />
+	<input type="hidden" name="integradoId" value="<?php echo $proyecto->integradoId; ?>">
+	<input type="hidden" name="status" 		value="<?php echo $proyecto->status ?>">
+	<input type="hidden" name="parentId" 	value="<?php echo $proyecto->parentId ?>">
+	<input type="hidden" name="id_proyecto" value="<?php echo $proyecto->id_proyecto ?>">
 
 	<div class="form-group">
 		<label for="name"><?php echo JText::_('COM_MANDATOS_PROYECTOS_LISTADO_TH_NAME_PROYECTO') ?></label>
 		<input type="text" name="name" id="name" maxlength="100" value="<?php echo $proyecto->name ?>">
 	</div>
-	
+
 	<div class="form-group">
 		<label for="description"><?php echo JText::_('COM_MANDATOS_PROYECTOS_LISTADO_DESCRIPCION_PROY') ?></label>
 		<textarea name="description" id="description" rows="10" maxlength="1000" style="width: 90%;"><?php echo $proyecto->description ?></textarea>
 	</div>
-	
+
 	<div class="form-actions">
 		<button type="submit" class="btn btn-primary span3" id="send"><?php echo JText::_('LBL_ENVIAR'); ?></button>
-        <a class="btn btn-danger span3" id="cancel" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=proyectoslist'); ?>"><?php echo JText::_('LBL_CANCELAR'); ?></a>
-		
-	</div>
+		<a class="btn btn-danger span3" id="cancel" href="<?php echo JRoute::_('index.php?option=com_mandatos&view=proyectoslist'); ?>"><?php echo JText::_('LBL_CANCELAR'); ?></a>
 
-    <div class="form-actions">
-        <button type="button" class="btn btn-default span3" id="btn-limpiar"><?php echo JText::_('LBL_LIMPIAR'); ?></button>
-    </div>
+	</div>
+	<?php if(is_null($proyecto->id_proyecto )){?>
+		<div class="form-actions">
+			<button type="button" class="btn btn-default span3" id="btn-limpiar"><?php echo JText::_('LBL_LIMPIAR'); ?></button>
+		</div>
+	<?php } ?>
 </form>
