6216a3403e400c93d95898e8a3544a47eff9e0b1
Ismael <nestor.aguilar@trama.mx>
2015-02-05 09:56:28 -0600

    Creacion de metodo envio de notificaciones, metodo que nos permite tener abstraccion en el envio

--- libraries/integradora/notifications.php
+++ libraries/integradora/notifications.php
@@ -12,6 +12,34 @@ class Send_email{
 
     public $responses;
     protected $recipient;
+    /*
+     * Esta funcion espera recibir tres parametros
+     * $notificaciones       = el numero de la notificacion que se llamara
+     * $data                 = Un arreglo de datos que contiene la informacion a cambiar en el texto
+     * $emailUserPrincipal   = Es el correo del usuario principal del integrado
+     * Retorna boolean true  = email enviado
+     *                 falce = error en envio y su descripcion;
+     */
+    public function sendNotifications($notification, $data, $emailUserPrincipal) {
+
+        $title  = 'TITLE_'.$notification;
+        $text   = 'NOTIFICACIONES_'.$notification;
+
+        $emails = array($emailUserPrincipal, JFactory::getUser()->email, 'aguilar_2001@hotmail.com');
+        $emails = array_unique($emails);
+
+        $titulo = JText::_($title);
+
+        $conten = JText::_($text);
+        $contenido = vsprintf($conten, $data);
+
+        $dato['titulo']         = $titulo;
+        $dato['body']           = $contenido;
+        $dato['email']          = $emails;
+        $send                   = new Send_email();
+        $info = $send->notification($dato);
+        return $info;
+    }
 
     public function notification($data){
 
@@ -26,7 +54,7 @@ class Send_email{
         foreach ($int->usuarios as $key => $val) {
             if(isset($val->permission_level)) {
                 if ($val->permission_level >= '3' || $val->id == JFactory::getUser()->id || $val->authorise('core.admin')) {
-                 return  $this->envia();
+                    return  $this->envia();
                 }
             }
         }
@@ -51,7 +79,7 @@ class Send_email{
         $mailer->setSubject($title);
         $mailer->setBody($body);
         $send = $mailer->Send();
-
+        var_dump($send);exit;
         $this->logEvent( $mailer, $send );
 
         return $send;
