c575d1c108d493664474ae32f8079a186d198505
Ricardo <ricardolyon@gmail.com>
2015-02-27 10:53:43 -0600

    [integradora/#654] Crear orden de retiro

--- components/com_mandatos/models/odvform.php
+++ components/com_mandatos/models/odvform.php
@@ -15,9 +15,9 @@ class MandatosModelOdvform extends JModelItem {
 
     public function __construct(){
         $this->inputVars   = JFactory::getApplication()->input->getArray( array('idOrden' => 'INT') );
-        $session           = JFactory::getSession();
+        $this->session     = JFactory::getSession();
         $this->catalogos   = new Catalogos();
-        $this->integradoId = $session->get( 'integradoId', null, 'integrado' );
+        $this->integradoId = $this->session->get( 'integradoId', null, 'integrado' );
 
         parent::__construct();
     }
@@ -69,7 +69,6 @@ class MandatosModelOdvform extends JModelItem {
 
     public function getProductos(){
         $respuesta = array();
-        $datos = new stdClass();
 
         $allproducts = getFromTimOne::getProducts($this->integradoId,null,1);
 
