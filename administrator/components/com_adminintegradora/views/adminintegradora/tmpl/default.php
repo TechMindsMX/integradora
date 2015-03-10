eaa0569d795e9ef72f2ff13a926c5188a6412abe
lutekValhalla <lutek.valhalla@gmail.com>
2015-03-10 09:30:24 -0600

    validación de datos bancarios en el mutuo

--- administrator/components/com_adminintegradora/views/adminintegradora/tmpl/default.php
+++ administrator/components/com_adminintegradora/views/adminintegradora/tmpl/default.php
@@ -7,20 +7,14 @@ $items = $this->items;
 
 ?>
 
-<a class="btn btn-large btn-primary" href="<?php echo JRoute::_('index.php?option=com_adminintegradora&view=comisions'); ?>">
-	<?php echo JText::_('COM_ADMININTEGRADORA_ADMIN_COMISIONES'); ?>
-</a>
+<div class="span2">
+    <a class="btn btn-large btn-primary" href="<?php echo JRoute::_('index.php?option=com_adminintegradora&view=comisions'); ?>">
+        <?php echo JText::_('COM_ADMININTEGRADORA_ADMIN_COMISIONES'); ?>
+    </a>
+</div>
 
-
-<?php
-$vars['MEDIA_FILES'] = defined('MEDIA_FILES');
-$vars['MIDDLE'] = defined('MIDDLE');
-$vars['PUERTO'] = defined('PUERTO');
-$vars['TIMONE'] = defined('TIMONE');
-$vars['TIMONE_ROUTE'] = defined('TIMONE_ROUTE');
-$vars['FACTURA_ROUTE'] = defined('FACTURA_ROUTE');
-$vars['SEPOMEX_SERVICE'] = defined('SEPOMEX_SERVICE');
-
-foreach ( $vars as $key => $constant ) {
-	echo '<p>'.$key .' = '. $constant.'</p>';
-}
+<div class="span2">
+    <a class="btn btn-large btn-primary" href="<?php echo JRoute::_('index.php?option=com_adminintegradora&view=comisions'); ?>">
+        <?php echo JText::_('COM_ADMININTEGRADORA_ADMIN_CONCILIACIONES'); ?>
+    </a>
+</div>
