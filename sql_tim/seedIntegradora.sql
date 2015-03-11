7877404d8645fe007cc401e2487ebf3c49976ef0
lutekValhalla <lutek.valhalla@gmail.com>
2015-03-11 16:37:25 -0600

    seed bancos y params

--- sql_tim/seedIntegradora.sql
+++ sql_tim/seedIntegradora.sql
@@ -1,4 +1,4 @@
-INSERT INTO `flpmu_integrado` (`integrado_id`, `status`, `pers_juridica`) VALUES ('1', '50', '1');
+INSERT INTO `flpmu_integrado` (`integrado_id`, `status`, `pers_juridica`) VALUES ('1', '1', '1');
 
 INSERT INTO `flpmu_integrado_users` (`integrado_id`, `user_id`, `integrado_principal`, `integrado_permission_level`) VALUES ('1', '93', b'1', '3');
 
@@ -7,3 +7,5 @@ INSERT INTO `flpmu_integrado_datos_personales` (`integrado_id`, `nombre_represen
 INSERT INTO `flpmu_integrado_datos_empresa` (`integrado_id`, `razon_social`, `rfc`, `calle`, `num_exterior`, `num_interior`, `cod_postal`, `tel_fijo`, `tel_fijo_extension`, `tel_fax`, `sitio_web`, `testimonio_1`, `testimonio_2`, `poder`, `reg_propiedad`, `url_rfc`) VALUES ('1', 'Integradora de Emprendimientos Culturales S.A. de C.V.', 'IEC121203FV8', 'Tiburcio Montiel', '80', 'B3', '11850', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `flpmu_integrado_datos_bancarios` (`datosBan_id` , `integrado_id` , `banco_codigo` , `banco_cuenta` ,`banco_sucursal` ,`banco_clabe` ,`banco_file`) VALUES (NULL ,  '1',  '014',  '0141805663',  '0141',  '014180566389265712', NULL);
+
+INSERT INTO  `integradb`.`flpmu_integrado_params` (`id` , `integrado_id` , `params`) VALUES (NULL ,  '1',  '1');
