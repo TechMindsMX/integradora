4eb36e73471055c6efb6f54ad979869633bf3397
Ricardo <ricardolyon@gmail.com>
2015-03-25 16:04:23 -0600

    Autorización ODC integrado - integrado

--- libraries/integradora/gettimone.php
+++ libraries/integradora/gettimone.php
@@ -3891,7 +3891,7 @@ class makeTx {
 
         jimport('joomla.log.log');
 
-        JLog::addLogger(array('text_file' => date('d-m-Y').'_bitacora_makeTxs.php', 'text_entry_format' => '{DATETIME} {PRIORITY} {MESSAGE} {CLIENTIP}'), JLog::INFO + JLog::DEBUG, 'bitacora');
+        JLog::addLogger(array('text_file' => date('Y-m-d').'_bitacora_makeTxs.php', 'text_entry_format' => '{DATETIME} {PRIORITY} {MESSAGE} {CLIENTIP}'), JLog::INFO + JLog::DEBUG, 'bitacora');
         $logdata = implode(' | ',array(JFactory::getUser()->id, JFactory::getSession()->get('integradoId', null, 'integrado'), __METHOD__, json_encode( array($this->objEnvio, $request) ) ) );
         JLog::add($logdata, JLog::DEBUG, 'bitacora');
 
