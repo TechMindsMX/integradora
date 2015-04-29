f010d8050bc49ba1bf0d489a7abf8ea1a12405a2
Ismael Aguilar <nestor.aguilar@trama.mx>
2015-04-29 15:42:31 -0500

    Notificaciones ODD Cobrada Admin

--- administrator/components/com_adminintegradora/controllers/conciliatxorder.php
+++ administrator/components/com_adminintegradora/controllers/conciliatxorder.php
@@ -6,6 +6,7 @@ use Integralib\Txs;
 
 jimport('joomla.application.component.controlleradmin');
 jimport('integradora.gettimone');
+jimport('integradora.notifications');
 /**
 *
 */
@@ -19,7 +20,8 @@ class AdminintegradoraControllerConciliaTxOrder extends JControllerAdmin {
 
         $conciliacion = new ConciliaTxOrder($tx,$order);
         $conciliacion->saveRelations();
-
+        $this->order = $order;
+        $this->sendEmail();
         JFactory::getApplication()->redirect('index.php?option=com_adminintegradora&view=oddlist');
     }
 
@@ -54,5 +56,51 @@ class AdminintegradoraControllerConciliaTxOrder extends JControllerAdmin {
         }
 
         return $order[0];
+    }
+
+    public function sendEmail(){
+        /*
+         *  NOTIFICACIONES 35
+         */
+
+        if($this->order->paymentMethod->id==1){
+            $metodoPago = JText::_('LBL_SPEI');
+        }
+        if($this->order->paymentMethod->id==2) {
+            $metodoPago = JText::_('LBL_DEPOSIT');
+        }
+        if($this->order->paymentMethod->id==3) {
+            $metodoPago = JText::_('LBL_CHEQUE');
+        }
+
+
+        $getCurrUser     = new IntegradoSimple($this->integradoId);
+        $titleArray      = array( $this->order->numOrden);
+        $name = $this->order->receptor;
+
+        $array           = array(
+            $name,
+            $this->order->numOrden,
+            date('d-m-Y'),
+            '$'.number_format($this->order->totalAmount, 2),
+            $metodoPago
+        );
+
+        $send            = new Send_email();
+
+        $send->setIntegradoEmailsArray($getCurrUser);
+        $info[]            = $send->sendNotifications('35', $array, $titleArray);
+
+        /*
+         * Notificaciones 9
+         */
+
+        $titleArrayAdmin = array( $name, $this->order->numOrden );
+        $arrayAdmin      = $array;
+
+        $send->setAdminEmails();
+        $info[] = $send->sendNotifications('36', $arrayAdmin, $titleArrayAdmin);
+
+        return $info;
     }
 }
