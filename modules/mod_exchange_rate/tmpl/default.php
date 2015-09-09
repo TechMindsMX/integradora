cc216e0ec9206b64c014c0513bce07b17e035d49
Ricardo <ricardolyon@gmail.com>
2015-09-09 16:15:07 -0500

    #35 formato de despliegue y etiquetas de idioma

--- modules/mod_exchange_rate/tmpl/default.php
+++ modules/mod_exchange_rate/tmpl/default.php
@@ -9,7 +9,25 @@
 
 defined('_JEXEC') or die;
 ?>
+<div class="row-fluid">
+    <div id="exchange-rate-module" class="<?php echo $moduleclass_sfx; ?> bg-warning box pull-left span3">
+        <div class="row-fluid">
+            <span class="span6">
+                <?php echo JText::_('EXCH_RATE_DATE'); ?>
+            </span>
+            <span class="span6 text-right">
+                <?php echo $exrate->fecha_tc; ?>
+            </span>
+        </div>
 
-<div id="exchange-rate-module" class="<?php echo $moduleclass_sfx; ?>">
-    <?php var_dump($exrate); ?>
+        <div class="row-fluid">
+            <span class="span6">
+                <?php echo JText::_('EXCH_RATE'); ?>
+            </span>
+            <span class="span6 text-right">
+                <?php echo $exrate->tc; ?> MXN/USD
+            </span>
+        </div>
+        <div class="text-center"><small><i><?php echo JText::_('EXCH_RATE_LEGEND'); ?></i></small></div>
+    </div>
 </div>
