27ec060bfbe9ebb887fe957c7b2fab7837cd5b17
Ricardo <ricardolyon@gmail.com>
2015-09-15 12:41:31 -0500

    #73 Se agregan constantes faltantes

--- libraries/Integralib/Environment.php
+++ libraries/Integralib/Environment.php
@@ -20,20 +20,33 @@ final class Environment
         $environment = $this->readEnviromentFile();
 
         if ( isset( $environment ) && !empty( $environment ) ) {
+//            integradora
             define('DEBUG', $environment['DEBUG']);
             define("MEDIA_FILES", $environment["MEDIA_FILES"]);
             define('XML_FILES_PATH', $environment['XML_FILES_PATH']);
+            define('INTEGRADORA_UUID', $environment['INTEGRADORA_UUID']);
+
+//            timone
             define("MIDDLE", 'http://' . $environment['MIDDLE']);
             define("PUERTO", $environment['PUERTOTIMONE']);
             define("TIMONE", $environment['CONTROLLERTIMONE']);
             define("TIMONE_ROUTE", MIDDLE . $environment['CONTROLLERTIMONE'] . 'integra/');
             define("TOKEN_ROUTE", MIDDLE . 'timone/oauth/');
             define('ENVIRONMENT_TIMONE', $environment['AMBIENTE_TIMONE']);
+
+//            sepomex
+            define('SEPOMEX_SERVICE', $environment['SEPOMEX_SERVICE']);
+
+//            oauth credentials
             define('OAUTH_USERNAME', $environment['OAUTH']['USERNAME']);
             define('OAUTH_PASSWORD', $environment['OAUTH']['PASSWORD']);
             define('OAUTH_CLIENT_ID', $environment['OAUTH']['CLIENT_ID']);
             define('OAUTH_CLIENT_SECRET', $environment['OAUTH']['CLIENT_SECRET']);
             define('OAUTH_GRANT_TYPE', $environment['OAUTH']['GRANT_TYPE']);
+
+//            invoicing
+            define('TOKEN_FACT_ROUTE', $environment['TOKEN_FACT_ROUTE']);
+            define('FACTURA_ROUTE', $environment['FACTURA_ROUTE']);
 
             SeedIntegradora::seedIntegradora('integradora-seed-'.$this->filename);
         } else {
