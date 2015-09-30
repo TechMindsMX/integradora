267559db04cfa5ae0e05398d9587f649df1b5583
Ricardo <ricardolyon@gmail.com>
2015-09-30 09:41:11 -0500

    #87 Se evitan los warnings de creacion de objeto en model mandatos

--- components/com_mandatos/models/mandatos.php
+++ components/com_mandatos/models/mandatos.php
@@ -7,14 +7,21 @@ jimport('integradora.integrado');
 jimport('integradora.rutas');
 jimport('integradora.catalogos');
 
-/**
- */
 class MandatosModelMandatos extends JModelItem {
 
+	public $data;
+
+	/**
+	 * @return mixed
+     */
 	function getAlta() {
 		$sesion = JFactory::getSession();
 		$integradoId = $sesion->get('integradoId', null, 'integrado');
 
+		$this->data = new stdClass();
+		$this->data->cliprov = new stdClass();
+		$this->data->project = new stdClass();
+		$this->data->product = new stdClass();
 		$this->data->cliprov->count = count(getFromTimOne::getClientes($integradoId));
 		$this->data->project->count = count(getFromTimOne::getProyects($integradoId));
 		$this->data->product->count = count(getFromTimOne::getProducts($integradoId));
