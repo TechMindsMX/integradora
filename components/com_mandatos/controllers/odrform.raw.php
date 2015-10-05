1d56fc82d3bd90d593d6a640548a373d2f2eed4e
Lutek <lutek.valhalla@gmail.com>
2015-10-05 12:04:06 -0500

    update Odr

--- components/com_mandatos/controllers/odrform.raw.php
+++ components/com_mandatos/controllers/odrform.raw.php
@@ -106,7 +106,7 @@ class MandatosControllerOdrform extends JControllerLegacy {
             $class->createPDF($data->datos, 'odr');
 
             if($class){
-                $save->updateDB('ordenes_retiro', array('urlPDFOrden = "'.$class->path.'"'), 'numOrden = '.$idOrden);
+                $save->updateDB('ordenes_retiro', array('urlPDFOrden = "'.$class->path.'"'), 'id = '.$idOrden);
             }
 
             $respuesta = array('urlRedireccion' => 'index.php?option=com_mandatos&view=odrpreview&idOrden=' . $idOrden.'&success=true',
